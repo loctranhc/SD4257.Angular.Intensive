7e506520879e13b0c91c114b596c783d03a6d5e5
Loc Tran <loctranhc@gmail.com>
2024-06-19 11:28:00 +0700

    feat: Add UpsertUser

--- SD4257.Angular.Intensive.Web/src/app/sign.up/sign.up.component.ts
+++ SD4257.Angular.Intensive.Web/src/app/sign.up/sign.up.component.ts
@@ -1,15 +1,47 @@
 import { Component } from '@angular/core';
-import {RouterLink} from "@angular/router";
+import {Router, RouterLink} from "@angular/router";
+import {FormsModule} from "@angular/forms";
+import {upsertAsync} from "../../services/user.service";
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    FormsModule
   ],
   templateUrl: './sign.up.component.html',
   styleUrl: './sign.up.component.css'
 })
 export class SignUpComponent {
+  fullName: string = '';
+  userName: string = '';
+  password: string = '';
 
+  constructor(private route: Router) {
+  }
+  
+  onFullNameChange = (value: any) => {
+    this.fullName = value;
+  }
+
+  onUserNameChange = (value: any) => {
+    this.userName = value;
+  }
+
+  onPasswordChange = (value: any) => {
+    this.password = value;
+  }
+  
+  onClickSubmit = ()=>{
+    upsertAsync(this.fullName, this.userName, this.password).then(respone => {
+      if(respone.status === 200){
+        this.route.navigate(['/login']);
+      }else {
+        alert(respone.statusText);
+      }
+    }).catch(error => {
+      console.log(error);
+    });
+  }
 }
